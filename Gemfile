b007d13a2f27f89fb26f1b4b4c2b411090f12269
Cody Stewart <cjstewart88@gmail.com>
2012-07-01 10:58:16 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerreads::Application.config.secret_token = 'c6de13530b0d30fae0f550d4c87d11dec8936379431cafa7e6f339fffb23ed3a338e3ecab258df3d33930b764cb17571ceda763b448a6e079c483675876bffa0'
