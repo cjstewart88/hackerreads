d74111666fdbd4fce4e9ec7a65cf7148ebb6b3d2
Cody Stewart <cjstewart88@gmail.com>
2012-07-01 23:03:15 -0400

    remove readme.rdoc and up the per page amount from 15 to 30

--- app/assets/javascripts/readsPaging.js
+++ app/assets/javascripts/readsPaging.js
@@ -3,12 +3,12 @@
     var self = this;
     
     $.each($(".read"), function(i) {
-      if (i+1 <= 15) $(this).fadeIn(2000);
+      if (i+1 <= 30) $(this).fadeIn(2000);
     });
     
     $(window).scroll(function() {
       if ($(window).scrollTop()+300 >= $(document).height() - $(window).height()) {
-        for (var i = 1; i <= 15; i++) {
+        for (var i = 1; i <= 30; i++) {
           $(".read").not(':hidden').last().next().fadeIn(2000);
         }
       }
