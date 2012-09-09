d691b8cbed992e22b8575e0cc03972bb2c490fad
Cody Stewart <cjstewart88@gmail.com>
2012-09-09 00:35:27 -0400

    upgrade sortByTimeAgo to fix a sorting bug

--- app/assets/javascripts/sortByTimeAgo.js
+++ app/assets/javascripts/sortByTimeAgo.js
@@ -1,37 +1,42 @@
 (function ($) {
   $.sortByTimeAgo = function (data) {
-    reads = [];
+    var sortedData    = [];
     
-    minutes = [];
-    hours   = [];
-    days    = [];
+    var seperatedData = {
+      secondsAgo: [],
+      minutesAgo: [],
+      hoursAgo:   [],
+      daysAgo:    [],
+      weeksAgo:   [],
+      monthsAgo:  [],
+      yearsAgo:   []
+    };
     
     $.each(data, function () {
-      this.timeAgo = this.timeAgo.replace("about", "");
-      
-      if (this.timeAgo.indexOf("minutes") != -1) {
-        minutes.push(this);
-      }
-      else if (this.timeAgo.indexOf("hours") != -1) {
-        hours.push(this);
-      }
-      else if (this.timeAgo.indexOf("days") != -1) {
-        days.push(this);
-      }
+      if      (this.timeAgo.indexOf("second") != -1) seperatedData.secondsAgo.push(this);      
+      else if (this.timeAgo.indexOf("minute") != -1) seperatedData.minutesAgo.push(this);
+      else if (this.timeAgo.indexOf("hour")   != -1) seperatedData.hoursAgo.push(this);
+      else if (this.timeAgo.indexOf("day")    != -1) seperatedData.daysAgo.push(this);
+      else if (this.timeAgo.indexOf("week")   != -1) seperatedData.weeksAgo.push(this);
+      else if (this.timeAgo.indexOf("month")  != -1) seperatedData.yearsAgo.push(this);
+      else if (this.timeAgo.indexOf("year")   != -1) seperatedData.monthsAgo.push(this);
     });
     
-    minutes.sort(function (a, b) {
-      return parseInt(a.timeAgo.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2')) - parseInt(b.timeAgo.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2'));
-    });
-    
-    hours.sort(function (a, b) {
-      return parseInt(a.timeAgo.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2')) - parseInt(b.timeAgo.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2'));
-    });
-    
-    days.sort(function (a, b) {
-      return parseInt(a.timeAgo.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2')) - parseInt(b.timeAgo.replace( /(^.+)(\w\d+\w)(.+$)/i,'$2'));
-    });
+    $.each(seperatedData, function () {
+      this.sort(function (a, b) {
+        var aTimeAgo = a.timeAgo.match(/\d+/);
+        var bTimeAgo = b.timeAgo.match(/\d+/);
 
-    return reads.concat(minutes, hours, days);
+        return parseInt(aTimeAgo == null ? 0 : aTimeAgo[0]) - parseInt(bTimeAgo == null ? 0 : bTimeAgo[0]);
+      });
+    });
+    
+    return sortedData.concat(seperatedData.secondsAgo, 
+                             seperatedData.minutesAgo, 
+                             seperatedData.hoursAgo, 
+                             seperatedData.daysAgo,
+                             seperatedData.weeksAgo,
+                             seperatedData.monthsAgo,
+                             seperatedData.yearsAgo);
   };
 })(jQuery);
